29bdc4083215e6bff6e8f4e3a7a2810e47b55ed1
Caleb Raney <110679107+calebraney@users.noreply.github.com>
2024-06-11 16:41:08 -0700

    initial interaction build

--- src/index.js
+++ src/index.js
@@ -1,29 +1,277 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Comment out for production
-  console.log('Local Script Loaded');
-
   // register gsap plugins if available
   if (gsap.ScrollTrigger !== undefined) {
     gsap.registerPlugin(ScrollTrigger);
   }
-  if (gsap.Flip !== undefined) {
-    gsap.registerPlugin(Flip);
-  }
 
-  //////////////////////////////
-  //Utility Functions
-  const attr = function (defaultVal, attrVal) {
-    const defaultValType = typeof defaultVal;
-    if (typeof attrVal !== 'string' || attrVal.trim() === '') return defaultVal;
-    if (attrVal === 'true' && defaultValType === 'boolean') return true;
-    if (attrVal === 'false' && defaultValType === 'boolean') return false;
-    if (isNaN(attrVal) && defaultValType === 'string') return attrVal;
-    if (!isNaN(attrVal) && defaultValType === 'number') return +attrVal;
-    return defaultVal;
+  const scrolling = function () {
+    //elements
+    const wrap = document.querySelector(`[data-scrolling="wrap"]`);
+    const nextWrap = document.querySelector(`[data-scrolling="next-wrap"]`);
+    const trigger = wrap;
+    const item1 = document.querySelector(`[data-scrolling="item-1"]`);
+    const item2 = document.querySelector(`[data-scrolling="item-2"]`);
+    const item3 = document.querySelector(`[data-scrolling="item-3"]`);
+    const circle1 = document.querySelector(`[data-scrolling="circle-1"]`);
+    const circle2 = document.querySelector(`[data-scrolling="circle-2"]`);
+    const circle3 = document.querySelector(`[data-scrolling="circle-3"]`);
+    const frame1 = document.querySelector(`[data-scrolling="frame-1"]`);
+    const frame2 = document.querySelector(`[data-scrolling="frame-2"]`);
+    const frame3 = document.querySelector(`[data-scrolling="frame-3"]`);
+    //check to make sure elements exist
+
+    if (
+      !trigger ||
+      !item1 ||
+      !item2 ||
+      !item3 ||
+      !circle1 ||
+      !circle2 ||
+      !circle3 ||
+      !frame1 ||
+      !frame2 ||
+      !wrap
+    )
+      return;
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: trigger,
+        start: 'top top',
+        end: 'bottom bottom',
+        ease: 'none',
+        markers: false,
+        scrub: true,
+      },
+      defaults: {
+        duration: 1,
+        ease: 'power1.out',
+      },
+    });
+    // STEP 1 - ANIMATE Circle 1 IN
+    // tl.set(item1, { zIndex: 2 });
+    tl.to(
+      item1,
+      {
+        width: '100vw',
+        height: '100vh',
+        margin: '0px 0px 0px 0px',
+        borderRadius: '0vw 0vw 0vw 0vw',
+      },
+      '<'
+    );
+    tl.to(
+      item3,
+      {
+        x: '120%',
+        opacity: 0,
+        duration: 0.4,
+      },
+      '<'
+    );
+    tl.to(
+      item2,
+      {
+        x: '100%',
+        opacity: 0,
+        duration: 0.5,
+      },
+      '<'
+    );
+    tl.to(
+      circle1,
+      {
+        opacity: 0,
+        duration: 0.5,
+      },
+      '<.25'
+    );
+    tl.to(
+      frame1,
+      {
+        opacity: 1,
+        duration: 0.5,
+      },
+      '<.25'
+    );
+    // STEP 2 - ANIMATE Circle 1 OUT
+    tl.to(frame1, {
+      opacity: 0,
+      duration: 0.5,
+      // scale: 0.8,
+      delay: 0.5,
+    });
+    tl.to(
+      item1,
+      {
+        width: '30vw',
+        height: '30vw',
+        marginRight: '50%',
+        borderRadius: '100vw 100vw 100vw 100vw',
+      },
+      '<'
+    );
+    tl.to(
+      circle1,
+      {
+        opacity: 1,
+        duration: 0.5,
+      },
+      '<.25'
+    );
+    tl.to(
+      item2,
+      {
+        x: '0%',
+        opacity: 1,
+        duration: 0.75,
+      },
+      '<'
+    );
+    tl.to(
+      item3,
+      {
+        x: '0%',
+        opacity: 1,
+        duration: 0.75,
+      },
+      '<'
+    );
+    // STEP 3 - ANIMATE Circle 2 IN
+    // tl.set(item2, { zIndex: 3 });
+    tl.to(item2, {
+      width: '100vw',
+      height: '100vh',
+      margin: '0px 0px 0px 0px',
+      borderRadius: '0vw 0vw 0vw 0vw',
+    });
+    tl.to(
+      item1,
+      {
+        x: '-100%',
+        opacity: 0,
+        duration: 0.5,
+      },
+      '<'
+    );
+    tl.to(
+      item3,
+      {
+        x: '100%',
+        opacity: 0,
+        duration: 0.5,
+      },
+      '<'
+    );
+    tl.to(
+      circle2,
+      {
+        opacity: 0,
+        duration: 0.5,
+      },
+      '<.25'
+    );
+    tl.to(
+      frame2,
+      {
+        opacity: 1,
+        duration: 0.5,
+      },
+      '<.25'
+    );
+    // STEP 4 - ANIMATE Circle 2 OUT
+    tl.to(frame2, {
+      opacity: 0,
+      duration: 0.5,
+      // scale: 0.8,
+      delay: 0.5,
+    });
+    tl.to(
+      item2,
+      {
+        width: '30vw',
+        height: '30vw',
+        marginRight: '0%',
+        borderRadius: '100vw 100vw 100vw 100vw',
+      },
+      '<'
+    );
+    tl.to(
+      circle2,
+      {
+        opacity: 1,
+        duration: 0.5,
+      },
+      '<.25'
+    );
+
+    tl.to(
+      item1,
+      {
+        x: '0%',
+        opacity: 1,
+      },
+      '<'
+    );
+    tl.to(
+      item3,
+      {
+        x: '0%',
+        opacity: 1,
+      },
+      '<'
+    );
+    // STEP 5 - ANIMATE Circle 3 IN
+    // tl.set(item3, { zIndex: 4 });
+    tl.to(item3, {
+      width: '100vw',
+      height: '100vh',
+      margin: '0px 0px 0px 0px',
+      borderRadius: '0vw 0vw 0vw 0vw',
+    });
+    tl.to(
+      item1,
+      {
+        x: '-120%',
+        opacity: 0,
+        duration: 0.4,
+      },
+      '<'
+    );
+    tl.to(
+      item2,
+      {
+        x: '-100%',
+        opacity: 0,
+        duration: 0.5,
+      },
+      '<'
+    );
+    tl.to(
+      circle3,
+      {
+        opacity: 0,
+        duration: 0.5,
+      },
+      '<.25'
+    );
+    tl.to(
+      wrap,
+      {
+        opacity: 0,
+        duration: 0.5,
+      },
+      '<'
+    );
+    tl.from(
+      nextWrap,
+      {
+        opacity: 0,
+        scale: 0.9,
+        duration: 0.25,
+      },
+      '<.25'
+    );
   };
-
-  const scrolling = function () {};
-
   //////////////////////////////
   //Control Functions on page load
   const gsapInit = function () {
@@ -39,7 +287,7 @@ document.addEventListener('DOMContentLoaded', function () {
       (gsapContext) => {
         let { isMobile, isTablet, isDesktop, reduceMotion } = gsapContext.conditions;
         //conditional interactions
-        if (!isDesktop) {
+        if (isDesktop || isTablet) {
           scrolling();
         }
       }
